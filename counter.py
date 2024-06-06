09af11572e6fd35526e4aaf7e93e94f03ddbd47e
Irina Danilova <53070156+IrinaDanilova-dev@users.noreply.github.com>
2024-06-06 13:42:50 +0300

    Update counter.py

--- counter.py
+++ counter.py
@@ -2,10 +2,14 @@ import time
 
 
 def main(qty):
+    pfile_name = './counter-data/data.txt'
+    file = open(pfile_name, "w")
+
     for i in range(qty):
         print(i)
+        file.write(f"iter {i} ")
         time.sleep(1)
+    file.close()
         
 main(15)
-        
         
